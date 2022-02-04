bdfe5c78c76f8dc8ab11df0501de60f96610c43f
Abdul <abdul-aziz99@live.com>
2022-02-03 22:01:57 -0500

    Added hamburger nav on mobile veiw. and scroll down on arrow

--- scripts.js
+++ scripts.js
@@ -2,11 +2,29 @@ const app = {};
 
 app.init = () => {
     app.menuListener();
+    app.scrollToAbout();
 }
 
 app.menuListener = () => {
     const menu = document.querySelector(".hamburger")
+    const navUL = document.querySelector("#navUL")
     menu.addEventListener("click", () => {
-        console.log("click clack")
+        if (navUL.classList.contains("show")){
+            navUL.classList.remove("show");
+        }
+        else {
+            navUL.classList.add("show");
+        }   
+        
     })
 }
+
+app.scrollToAbout = () => {
+    const button = document.querySelector("#scrollToAbout");
+    const about = document.querySelector("#about");
+    button.addEventListener("click", () => {
+        about.scrollIntoView({ behavior: "smooth", block: "start"});
+    })
+}
+
+app.init();
